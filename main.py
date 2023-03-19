f7fcc860943bdf546cff0844ba29f5194e431e9c
İlker Kaya <ilkerkaya@192.168.1.36>
2023-03-19 14:13:51 +0300

    Finished queary and path parameters

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {'data': {'name':'Sarthak'}}
+@app.get("/blog")
+def index(limit=10, published:bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
-@app.get("/about")
-def about():
-    return {'data': {'about page'}}
+@app.get('/blog/unpublished')
+def unpublisched():
+    return {'data':'all published blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    #fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id = id
+    return {'data':{"comments":"1"}}
